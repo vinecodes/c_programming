c778c65c1ea3017b537d8a715e052a7fde88b07d
Vineeth Penugonda <vineeth.pen@gmail.com>
2025-07-26 20:08:57 +0530

    fix: correct pointer usage and improve output formatting in memory.c

--- memory.c
+++ memory.c
@@ -13,14 +13,16 @@ int main(int argc, char **argv)
 
         *(memory + i) = 10 - i;
 
-        char *p_read_bytes = (char *)memory;
+        printf("%d:\t%d\t%p\n", i, *(memory + i), memory + i);
+
+        char *p_read_bytes = (char *)(memory + i);
 
         for (int i = 0; i < 4; i++)
         {
             printf("Data at %p: %02X\n", p_read_bytes + i, *(p_read_bytes + i));
         }
 
-        printf("%d:\t%d\t%p\n", i, *(memory + i), memory + i);
+        printf("\n");
     }
 
     // Important or else Heap is going to be filled up ;)
